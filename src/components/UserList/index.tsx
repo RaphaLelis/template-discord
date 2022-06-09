991d8392ce108ea2c15cbd6db3678fa19ea0165e
Raphael Lelis <raphalelis@outlookc.com>
2022-06-09 19:17:30 -0300

    add userList in app

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Role, User, Avatar} from './styles';
+
+export interface Props{
+	nickname: string,
+	isBot?: boolean 
+}
+
+const UserRown: React.FC<Props> = ({nickname, isBot}) =>{
+	return (
+		<User>
+			<Avatar className={isBot ? 'bot' : ''}/>
+			<strong>{nickname}</strong>
+
+			{isBot && <span>Bot</span>}
+		</User>
+
+	)
+}
+const UserList: React.FC = () =>{
+	return (
+		<Container>
+			<Role>Disponível - 1</Role>
+			<UserRown nickname = "Adulto Lelis"/>
+
+			<Role>Offline - 25</Role>
+			<UserRown nickname = "Jukebox do seu Zé" isBot/>
+		</Container>
+	);
+};
+
+export default UserList;
